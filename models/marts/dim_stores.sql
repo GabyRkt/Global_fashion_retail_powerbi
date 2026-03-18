69e87deb77dda3e0aeec5dba5ef44f71eab9b1f9
Gabriella Rakotoarison <gabirakt@gmail.com>
2026-03-18 09:39:20 +0000

    Change foreign names in stores' country

--- models/marts/dim_stores.sql
+++ models/marts/dim_stores.sql
@@ -6,17 +6,22 @@ select
     store_id,
     store_name,
     city,
-    country,
+    case
+        when country = '中国' then 'China'
+        when country = 'Deutschland' then 'Germany'
+        when country = 'España' then 'Spain'
+        else country
+    end as country,
     number_of_employees,
     zip_code,
     latitude,
     longitude,
     case
-        when country in ('France', 'Germany', 'Spain', 'Portugal') then 'Europe Continentale'
+        when country in ('France', 'Deutschland', 'España', 'Portugal') then 'Continental Europe'
         when country = 'United Kingdom' then 'Europe UK'
-        when country = 'United States' then 'Amérique du Nord'
-        when country = 'China' then 'Asie'
-        else 'Autre'
+        when country = 'United States' then 'North America'
+        when country = '中国' then 'Asia'
+        else 'Other'
     end as region
 
 from stg_stores
